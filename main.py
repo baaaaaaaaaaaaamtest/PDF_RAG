82594a14d073090bd005d47394bdc15afd93e38b
unknown <ansgyqja@gmail.com>
2025-10-27 12:30:11 +0900

    modify: streamlit 연동 및 테스트 완료 단일 파일 기준 처리 이미지 로드 등 개선사항 존재

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import streamlit as st
 from langchain_teddynote import logging
 from dotenv import load_dotenv
 from langchain_core.messages.chat import ChatMessage
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_teddynote.prompts import load_prompt
-from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from modules.utils import *
+from modules.prompt import *
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 logging.langsmith("[Project] Upstage PDF RAG")
-
 
 # 캐시 디렉토리 생성
 if not os.path.exists(".cache"):
@@ -45,8 +45,6 @@ with st.sidebar:
     # 파일 업로드
     uploaded_file = st.file_uploader("파일 업로드", type=["pdf"])
 
-    # uploaded_btn_apply = st.button("pdf 분석")
-
     # 모델 선택 메뉴
     selected_model = st.selectbox(
         "LLM 선택", ["gpt-4o", "gpt-4-turbo", "gpt-4o-mini"], index=2
@@ -66,48 +64,59 @@ def add_message(role, message):
     st.session_state["messages"].append(ChatMessage(role=role, content=message))
 
 
-# 파일을 캐시 저장(시간이 오래 걸리는 작업을 처리할 예정)
-# @st.cache_resource(show_spinner="업로드한 파일을 처리 중입니다...")
 def embed_file(file):
-    print(file)
-    # 업로드한 파일을 캐시 디렉토리에 저장합니다.
-    file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-    output_folder = os.path.splitext(file_path)[0]
-    print(file_path)
-    with open(file_path, "wb") as f:
-        f.write(file_content)
+    with st.spinner("업로드한 파일을 처리 중입니다..."):
+        file_content = file.read()
+        file_path = f"./.cache/files/{file.name}"
+        embedding_path = f"./.cache/embeddings/{file.name}"
+        embedding_basename = os.path.splitext(embedding_path)[0]
+        with open(file_path, "wb") as f:
+            f.write(file_content)
 
-    # 분석 방법 선택
-    if selected_method == "normal":
-        print("normal")
-        retriever = noraml_retrievr(file_path)
-    elif selected_method == "upstage":
-        print("upstage")
-        final_output_path = f"{output_folder}\\analysis.html"
-        if os.path.isfile(final_output_path):
-            print("File exists.")
-        else:
-            print("File does not exists.")
-            split_files = split_pdf(file_path)
-            analyser = LayoutAnalyzer(os.environ.get("UPSTAGE_API_KEY"))
-            analyser.execute(split_files)  # json 파일로 변환하여 저장 완료
-            image_processor = PDFImageProcessor(file_path)
-            html_content = image_processor.extract_images()
-            add_summary_to_html(html_content, image_processor.output_folder)
-            print("변환 완료")
-        docs = get_docs(output_folder)
-        faiss_retriever = get_retriever(docs)
-        bm25_retriever = get_bm25_retriever(docs)
-        retriever = get_esenmble_retriever(faiss_retriever, bm25_retriever)
-
+        # 분석 방법 선택
+        if selected_method == "normal":
+            print("normal")
+            retriever = noraml_retrievr(file_path)
+        elif selected_method == "upstage":
+            print("upstage")
+            if os.path.exists(embedding_basename):
+                print("이미 존재")
+                retriever = load_retreiver(embedding_basename)
+            else:
+                print("생성 필요")
+                file_paths, basename = split_pdf(file_path)  # 파일 분할
+                analysis_file_path = upstage_layout_analysis(
+                    file_paths
+                )  # upstage 파일 분석
+                element_content = reOrder_id(analysis_file_path)  # order_id 재배치
+                _element_content = extract_image(
+                    element_content, basename
+                )  # 이미지 저장
+                image_urls, system_prompts, user_prompts = order_image_prompt(
+                    _element_content
+                )  # 이미지 summary 전처리
+                image_answer = get_image_summary(
+                    image_urls, system_prompts, user_prompts
+                )  # summar from llm
+                change_element_content = add_image_description(
+                    image_answer, _element_content
+                )
+                system_prompts, user_prompts = order_equation_prompt(
+                    change_element_content
+                )
+                equation_answer = request_llm_equation(system_prompts, user_prompts)
+                final_content = add_eqaution_description(
+                    equation_answer, change_element_content
+                )
+                docs = change_json_to_document(final_content)
+                retriever = get_retriever(docs, embedding_path)
+                # bm25_retriever = get_bm25_retriever(docs)
+                # retriever = get_esenmble_retriever(faiss_retriever, bm25_retriever)
     return retriever
 
 
 # 체인 생성
 def create_chain(model_name="gpt-4o"):
-    # 단계 7: 언어모델(LLM) 생성
-    # 모델(LLM) 을 생성합니다.
     llm = ChatOpenAI(model_name=model_name, temperature=0)
     return llm
 
@@ -128,7 +137,7 @@ if uploaded_file:
 if clear_btn:
     st.session_state["messages"] = []
 
-
+print_messages()
 # 사용자의 입력
 user_input = st.chat_input("궁금한 내용을 물어보세요!")
 
@@ -140,23 +149,16 @@ if user_input:
     # chain 을 생성
     llm = st.session_state["llm"]
     if llm is not None:
-        # 사용자의 입력
         st.chat_message("user").write(user_input)
-        # 스트리밍 호출
-        # 단계 6: 프롬프트 생성(Create Prompt)
-        # 프롬프트를 생성합니다.
-        prompt = load_prompt("prompts/pdf-rag.yaml", encoding="utf-8")
+        prompt = get_prompt_user_request()
         retriever = st.session_state["retriever"]
         reranker_docs = get_reranker(retriever, user_input)
         reorder_docs = reorder_documents(reranker_docs)
-        # print(reorder_docs)
-        # print(user_input)
         chain = prompt | llm | StrOutputParser()
         response = chain.stream({"question": user_input, "context": reorder_docs})
         with st.chat_message("assistant"):
             # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
             container = st.empty()
-
             ai_answer = ""
             for token in response:
                 ai_answer += token
--- modules/utils.py
+++ modules/utils.py
@@ -79,6 +79,7 @@ def split_pdf(filepath, batch_size=5):
 
     return ret, basename
 
+
 def upstage_layout_analysis(input_files):
     analyzed_files = []
     config = {
@@ -108,11 +109,11 @@ def upstage_layout_analysis(input_files):
 
 
 def reOrder_id(analysis_file_path):
-    """ 
-        element 구성 변경
-        id 정렬
-        html 값 변경
-        page 값 수정
+    """
+    element 구성 변경
+    id 정렬
+    html 값 변경
+    page 값 수정
     """
     element_content = []
     page_range = 5
@@ -134,9 +135,10 @@ def reOrder_id(analysis_file_path):
             element_content.extend(data["elements"])
     return element_content
 
-def extract_image(element_content,basename):
-    """ 
-        element_content 에서 base64 정보 가져와 이미지로 저장
+
+def extract_image(element_content, basename):
+    """
+    element_content 에서 base64 정보 가져와 이미지로 저장
     """
     arr = []
     for idx, item in enumerate(element_content):
@@ -175,14 +177,20 @@ def extract_image(element_content,basename):
 
 def order_image_prompt(_element_content):
     """
-        이미지를 LLM에 전달하기전 프롬프트, 이미지주소 정렬
+    이미지를 LLM에 전달하기전 프롬프트, 이미지주소 정렬
     """
     image_urls: list[str] = []
     system_prompts: list[str] = []
     user_prompts: list[str] = []
     for idx, item in enumerate(_element_content):
-        previous_context = _element_content[idx - 1]["content_text"] if idx - 1 >= 0 else None
-        next_context = _element_content[idx + 1]["content_text"] if idx + 1 < len(_element_content) else None
+        previous_context = (
+            _element_content[idx - 1]["content_text"] if idx - 1 >= 0 else None
+        )
+        next_context = (
+            _element_content[idx + 1]["content_text"]
+            if idx + 1 < len(_element_content)
+            else None
+        )
         id_str = item["metadata"]["id"]
         html_str = item["content_text"]
         soup = BeautifulSoup(html_str, "html.parser")
@@ -194,12 +202,20 @@ def order_image_prompt(_element_content):
             user_prompts.append(
                 get_prompt_user_image(previous_context, next_context, html_str, id_str)
             )
-    return image_urls,system_prompts,user_prompts
+    return image_urls, system_prompts, user_prompts
 
 
-def change_image_text_to_summary(answer,_element_content):
-    """ 
-        llm 에서 받은 answer를 기반으로 해당 element 찾아 answer 추가
+def get_image_summary(image_urls, system_prompts, user_prompts):
+    llm = get_gpt()
+    multimodal_llm = MultiModal(llm)
+    return multimodal_llm.batch(
+        image_urls, system_prompts, user_prompts, display_image=False
+    )
+
+
+def add_image_description(answer, _element_content):
+    """
+    llm 에서 받은 answer를 기반으로 해당 element 찾아 answer 추가
     """
     for html_str in answer:
         soup = BeautifulSoup(html_str, "html.parser")
@@ -216,14 +232,20 @@ def change_image_text_to_summary(answer,_element_content):
 
 def order_equation_prompt(change_element_content):
     """
-        방정식 전후 내용을 포함하여 프롬프트, 내용 정렬 
+    방정식 전후 내용을 포함하여 프롬프트, 내용 정렬
     """
     system_prompts: list[str] = []
     user_prompts: list[str] = []
 
     for idx, item in enumerate(change_element_content):
-        previous_context = change_element_content[idx - 1]["content_text"] if idx - 1 >= 0 else None
-        next_context = change_element_content[idx + 1]["content_text"] if idx + 1 < len(change_element_content) else None
+        previous_context = (
+            change_element_content[idx - 1]["content_text"] if idx - 1 >= 0 else None
+        )
+        next_context = (
+            change_element_content[idx + 1]["content_text"]
+            if idx + 1 < len(change_element_content)
+            else None
+        )
         id_str = item["metadata"]["id"]
         html_str = item["content_text"]
         soup = BeautifulSoup(html_str, "html.parser")
@@ -235,7 +257,8 @@ def order_equation_prompt(change_element_content):
                     previous_context, next_context, equation_tag.get_text(), id_str
                 )
             )
-    return system_prompts,user_prompts
+    return system_prompts, user_prompts
+
 
 def create_messages(system_prompt, user_prompt):
     messages = [
@@ -255,9 +278,10 @@ def create_messages(system_prompt, user_prompt):
     ]
     return messages
 
+
 def request_llm_equation(system_prompts, user_prompts):
-    """ 
-        llm에게 방정식에대한 설명 요청
+    """
+    llm에게 방정식에대한 설명 요청
     """
     messages = []
     for system_prompt, user_prompt in zip(system_prompts, user_prompts):
@@ -267,9 +291,9 @@ def request_llm_equation(system_prompts, user_prompts):
     return llm.batch(messages)
 
 
-def add_eqaution_description(answer,change_element_content):
-    """ 
-        llm 에서 받은 answer를 기반으로 해당 element 찾아 answer 추가
+def add_eqaution_description(answer, change_element_content):
+    """
+    llm 에서 받은 answer를 기반으로 해당 element 찾아 answer 추가
     """
     for r in answer:
         html_str = r.content
@@ -288,313 +312,29 @@ def add_eqaution_description(answer,change_element_content):
     return change_element_content
 
 
-class PDFImageProcessor:
-    """
-    PDF 이미지 처리를 위한 클래스
+def change_json_to_document(final_content):
+    docs = []
+    for d in final_content:
+        doc = Document(page_content=d["content_text"], metadata=d["metadata"])
+        docs.append(doc)
+    return docs
 
-    PDF 파일에서 이미지를 추출하고, HTML 및 Markdown 형식으로 변환하는 기능을 제공합니다.
-    """
-
-    def __init__(self, pdf_file):
-        """
-        PDFImageProcessor 클래스의 생성자
-
-        :param pdf_file: 처리할 PDF 파일의 경로
-        """
-        self.pdf_file = pdf_file
-        self.json_files = sorted(glob(os.path.splitext(pdf_file)[0] + "*.json"))
-        self.output_folder = os.path.splitext(pdf_file)[0]
-        self.filename = os.path.splitext(os.path.basename(pdf_file))[0]
-
-    @staticmethod
-    def _load_json(json_file):
-        """
-        JSON 파일을 로드하는 정적 메서드
-
-        :param json_file: 로드할 JSON 파일의 경로
-        :return: JSON 데이터를 파이썬 객체로 변환한 결과
-        """
-        with open(json_file, "r", encoding="utf-8") as f:
-            return json.load(f)
-
-    @staticmethod
-    def _get_page_sizes(json_data):
-        """
-        각 페이지의 크기 정보를 추출하는 정적 메서드
-
-        :param json_data: JSON 데이터
-        :return: 페이지 번호를 키로, [너비, 높이]를 값으로 하는 딕셔너리
-        """
-        page_sizes = {}
-        for page_element in json_data["metadata"]["pages"]:
-            width = page_element["width"]
-            height = page_element["height"]
-            page_num = page_element["page"]
-            page_sizes[page_num] = [width, height]
-        return page_sizes
-
-    def pdf_to_image(self, page_num, dpi=300):
-        """
-        PDF 파일의 특정 페이지를 이미지로 변환하는 메서드
-
-        :param page_num: 변환할 페이지 번호 (1부터 시작)
-        :param dpi: 이미지 해상도 (기본값: 300)
-        :return: 변환된 이미지 객체
-        """
-        with pymupdf.open(self.pdf_file) as doc:
-            # print(doc)
-            page = doc[page_num - 1].get_pixmap(dpi=dpi)
-            target_page_size = [page.width, page.height]
-            page_img = Image.frombytes("RGB", target_page_size, page.samples)
-            # page_img.save(f'{page_num}' + ".png")  # 이미지 저장
-        return page_img
-
-    @staticmethod
-    def normalize_coordinates(coordinates, output_page_size):
-        """
-        좌표를 정규화하는 정적 메서드
-
-        :param coordinates: 원본 좌표 리스트
-        :param output_page_size: 출력 페이지 크기 [너비, 높이]
-        :return: 정규화된 좌표 (x1, y1, x2, y2)
-        """
-        x_values = [coord["x"] for coord in coordinates]
-
-        y_values = [coord["y"] for coord in coordinates]
-        x1, y1, x2, y2 = min(x_values), min(y_values), max(x_values), max(y_values)
-
-        return (
-            x1 / output_page_size[0],
-            y1 / output_page_size[1],
-            x2 / output_page_size[0],
-            y2 / output_page_size[1],
-        )
-
-    @staticmethod
-    def crop_image(img, coordinates, output_file):
-        """
-        pdf 파일에서 얻은 좌표의 위치를 정규화하여 비율로 변환 한 coordinates
-        그 이후 pdf 파일을 이미지화
-        이미지의 크기를 사용하여 정규화 값과 곱하여 이미지속 좌표를 재생산하여 크롭하는 로직
-
-
-        :param img: 원본 이미지 객체
-        :param coordinates: 정규화된 좌표 (x1, y1, x2, y2)
-        :param output_file: 저장할 파일 경로
-        """
-        img_width, img_height = img.size
-        x1, y1, x2, y2 = [
-            int(coord * dim)
-            for coord, dim in zip(coordinates, [img_width, img_height] * 2)
-        ]
-        cropped_img = img.crop((x1, y1, x2, y2))
-        cropped_img.save(output_file)
-
-    def change_image_path(self, element, output_file, output_folder):
-        """
-        upstage 에서 받아온 json 파일의 target의 img 태그 src 를 sample-report 폴더 경로로 변경하는 코드
-
-        Args:
-        element : upstage에서 받아온 내부에 저장된 json
-        output_file : 파일명
-        output_folder : 파일 경로
-
-        Return : 변경된 파일 경로
-        """
-        soup = BeautifulSoup(element["html"], "html.parser")
-        img_tag = soup.find("img")
-        # print(img_tag)
-        if img_tag:
-            # 상대 경로로 변경
-            relative_path = os.path.relpath(output_file, output_folder)
-            img_tag["src"] = relative_path.replace("\\", "/")
-        return str(soup)
-
-    def change_add_id(self, data_list):
-        html_content = []
-        for i, data in enumerate(data_list):
-            soup = BeautifulSoup(data, "html.parser")
-
-            for tag in soup.find_all(attrs={"id": True}):
-                tag["id"] = str(i)
-                html_content.append(str(tag))
-        return html_content
-
-    def extract_images(self):
-        """
-        전체 이미지 처리 과정을 실행하는 메서드
-
-        PDF에서 이미지를 추출하고, HTML 및 Markdown 파일을 생성합니다.
-        """
-        figure_count = {}  # 페이지별 figure 카운트를 저장하는 딕셔너리
-
-        output_folder = self.output_folder
-
-        # return output_folder
-        os.makedirs(output_folder, exist_ok=True)
-
-        print(f"폴더가 생성되었습니다: {output_folder}")
-        html_content = []  # HTML 내용을 저장할 리스트
-        for json_file in self.json_files:
-            json_data = self._load_json(json_file)
-            page_sizes = self._get_page_sizes(json_data)
-            # print(f"page_sizes : {page_sizes}")
-            # 파일 이름에서 페이지 범위 추출
-            page_range = os.path.basename(json_file).split("_")[1:]
-            # print(f"page_range : {page_range}")
-            start_page = int(page_range[0])
-            # print(f"start_page : {start_page}")
-            for element in json_data["elements"]:
-                if element["category"] == "chart":
-                    # 파일 내에서의 상대적인 페이지 번호 계산
-                    relative_page = element["page"]
-                    # print(f"relative_page : {relative_page}")
-                    page_num = start_page + relative_page
-                    # print(f"page_num : {page_num}")
-                    coordinates = element["bounding_box"]
-                    # print(f"coordinates : {coordinates}")
-                    output_page_size = page_sizes[relative_page]
-                    # print(f"output_page_size : {output_page_size}")
-                    pdf_image = self.pdf_to_image(page_num)
-                    # print(pdf_image)
-                    normalized_coordinates = self.normalize_coordinates(
-                        coordinates, output_page_size
-                    )
-                    # print(f"normalized_coordinates : {normalized_coordinates}")
-                    # 페이지별 figure 카운트 관리
-                    if page_num not in figure_count:
-                        figure_count[page_num] = 1
-                    else:
-                        figure_count[page_num] += 1
-
-                    # 출력 파일명 생성
-                    output_file = os.path.join(
-                        output_folder,
-                        f"page_{page_num}_chart_{figure_count[page_num]}.png",
-                    )
-                    # print(f"이미지 저장됨: {output_file}")
-
-                    self.crop_image(pdf_image, normalized_coordinates, output_file)
-                    # HTML에서 이미지 경로 업데이트
-                    element["html"] = self.change_image_path(
-                        element, output_file, output_folder
-                    )
-                    # print(element["html"])
-                html_content.append(element["html"])
-        html_content = self.change_add_id(html_content)
-        # print(html_content)
-        # # HTML 파일 저장
-        html_output_file = os.path.join(output_folder, f"{self.filename}.html")
-
-        # print(f'html_content : {html_content[0]}')
-        # print(f'html_content : {html_content[15]}')
-        # print(f'html_content : {len(html_content)}')
-
-        combined_html_content = "\n".join(
-            [item for item in html_content if item is not None]
-        )
-        soup = BeautifulSoup(combined_html_content, "html.parser")
-        all_tags = set([tag.name for tag in soup.find_all()])
-        html_tag_list = [tag for tag in list(all_tags) if tag not in ["br"]]
-
-        with open(html_output_file, "w", encoding="utf-8") as f:
-            f.write(combined_html_content)
-
-        print(f"HTML 파일이 {html_output_file}에 저장되었습니다.")
-
-        # # Markdown 파일 저장
-        md_output_file = os.path.join(output_folder, f"{self.filename}.md")
-
-        md_output = markdown(
-            combined_html_content,
-            convert=html_tag_list,
-        )
-
-        with open(md_output_file, "w", encoding="utf-8") as f:
-            f.write(md_output)
-
-        print(f"Markdown 파일이 {md_output_file}에 저장되었습니다.")
-        return combined_html_content
-
-
-def get_summary_from_ai(image_paths, previous_context, next_context):
-    llm = get_gpt()
-    system_prompt = """You are an expert in extracting useful information from IMAGE.
-    With a given image, your task is to extract key entities, summarize them, and write useful information that can be used later for retrieval."""
-    user_prompt_template = f"""Here is the context related to the image: 
-    previous text : {previous_context}
-    
-    next text : {next_context}
-    
-    ###
-    Output Format:
-    <image>
-    <title>
-    <summary>
-    <entities> 
-    <path> {image_paths} </path>
-    </image>
-    """
-    multimodal_llm = MultiModal(llm)
-    answer = multimodal_llm.invoke(
-        image_paths, system_prompt, user_prompt_template, display_image=False
-    )
-    return answer
-
-
-def add_summary_to_html(html_content, output_folder):
-    soup = BeautifulSoup(html_content, "html.parser")
-    tags = sorted(soup.find_all(attrs={"id": True}), key=lambda x: int(x["id"]))
-    for tag in tags:
-        if tag.get("data-category") == "chart":
-            img_tag = tag.find("img")
-            if img_tag and img_tag.has_attr("src"):
-                cur_id = int(tag.get("id"))
-                image_path = img_tag["src"]
-                pre_tag = soup.find(id=cur_id - 1)
-                next_tag = soup.find(id=cur_id + 1)
-                summary = get_summary_from_ai(
-                    f"{output_folder}/{image_path}",
-                    pre_tag.get_text(),
-                    next_tag.get_text(),
-                )
-                # print(summary)
-
-                # img 태그는 유지하면서 요약 텍스트를 새로운 <p> 태그로 추가
-                summary_tag = soup.new_tag("p")
-                summary_tag.string = summary
-                img_tag.insert_after(summary_tag)
-
-    with open(f"{output_folder}/analysis.html", "w", encoding="utf-8") as f:
-        f.write(str(soup))
-
-
-def get_docs(output_folder):
-    with open(f"{output_folder}/analysis.html", "r", encoding="utf-8") as f:
-        html_content = f.read()
-    headers_to_split_on = [
-        ("h1", "Header 1"),  # 분할할 헤더 태그와 해당 헤더의 이름을 지정합니다.
-        ("h2", "Header 2"),
-        ("h3", "Header 3"),
-    ]
-    # 지정된 헤더를 기준으로 HTML 텍스트를 분할하는 HTMLHeaderTextSplitter 객체를 생성합니다.
-    html_splitter = HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-    # HTML 문자열을 분할하여 결과를 html_header_splits 변수에 저장합니다.
-    html_header_splits = html_splitter.split_text(html_content)
-    # 분할된 결과를 출력합니다.
-    last_segment = os.path.basename(output_folder) + "analysis.html"
-    documents = []
-    for chunk in html_header_splits:
-        doc = Document(
-            page_content=chunk.page_content, metadata={"source": last_segment}
-        )
-        documents.append(doc)
-    return documents
-
-
-def get_retriever(split_docs, model="text-embedding-3-small"):
+def get_retriever(split_docs, embedding_path, model="text-embedding-3-small"):
+    # PDF 파일 열기
+    basename = os.path.splitext(embedding_path)[0]
     embeddings = OpenAIEmbeddings(model=model)
     db = FAISS.from_documents(documents=split_docs, embedding=embeddings)
+    db.save_local(basename)
+    return db.as_retriever(
+        search_type="mmr", search_kwargs={"k": 5, "lambda_mult": 0.25, "fetch_k": 10}
+    )
+
+
+def load_retreiver(embedding_basename, model="text-embedding-3-small"):
+    embeddings = OpenAIEmbeddings(model=model)
+    db = FAISS.load_local(
+        embedding_basename, embeddings, allow_dangerous_deserialization=True
+    )
     return db.as_retriever(
         search_type="mmr", search_kwargs={"k": 5, "lambda_mult": 0.25, "fetch_k": 10}
     )
